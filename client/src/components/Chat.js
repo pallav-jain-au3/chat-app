9aad2a4e9b8dd2bfd510e5e7264829774cba95c9
pallav-jain-au3 <pallavjain409.com>
2020-05-03 13:04:59 +0530

    Add Functionality to Join Component

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import io from "socket.io-client"
+import queryString from 'query-string'
 
-const Chat = () => {
+const Chat = ({location}) => {
+	let socket;
+	let endpoint = 'localhost:5000';
+	const [name, setName] = useState('')
+	const [room, setRoom] = useState('')
+		useEffect(() => {
+			const {name, room} = queryString.parse(location.search)
+			socket = io(endpoint)
+			setName(name)
+			setRoom(room)
+		}, [])
 	return (
+		
 		<div>
 			<h1>Chat Room</h1>
 		</div>)
--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,9 +1,39 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import './Join.css'
 const Join = () => {
+	const [name, setName] = useState('')
+	const [room, setRoom] = useState('')
 	return (
-		<div>
-			<h1>Join</h1>
+		<div className = 'joinOuterContainer'>
+			<div className = "joinInnerContainer">
+				<h1 className = "heading">Join</h1>
+				<div>
+					<input
+					className = "joinInput"
+					placeholder = "Name" 
+					name = "name" 
+					type = "text" 
+					onChange = {(e) => setName(e.target.value)} 
+					value = {name} />
+				 </div>
+				 <div>
+					<input
+					className = "joinInput mt-20"
+					placeholder = "Room" 
+					name = "room" 
+					type = "text" 
+					onChange = {(e) => setRoom(e.target.value)} 
+					value = {room} />
+				 </div>
+				 <Link onClick = {e => !name || !room ? e.preventDefault() : null}
+				 	to = {`/chat?name=${name}&room=${room}`}>
+				 	<button type = 'sumbit'
+				 	className = "button mt-20">
+				 	Sign in
+				 	</button>
+				 </Link>
+			</div>
 		</div>)
 }
 
